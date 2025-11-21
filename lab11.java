f7f8c4f40eed0e9b1f74aaa65b8c57e7c19a8a03
kittinun12 <hougohum33@gmail.com>
2025-11-21 12:35:31 +0700

    ggg

---
+++ lab11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class lab11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter interger");
+        int a = sc.nextInt();   // รับค่าตัวเลขตัวแรก
+        System.out.println("Enter interger too");
+        int b = sc.nextInt();   // รับค่าตัวเลขตัวที่สอง
+
+        int sum = a + b;        // คำนวณผลบวก
+
+        System.out.println(sum); // แสดงผล
+    }
+}
