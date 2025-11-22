bc5ab00f123d67b882bf573f6a024ad7a5dfbd6d
kittinun12 <hougohum33@gmail.com>
2025-11-22 16:53:56 +0700

    update3

--- lab13.java
+++ lab13.java
@@ -1,27 +1,20 @@
-/*
- * โปรแกรมตรวจสอบว่าเลขเป็น "Even" หรือ "Odd"
- * ผู้ใช้ป้อนจำนวนเต็ม 1 จำนวน
- * ถ้า num % 2 == 0 → แสดง Even
- * ถ้าไม่ใช่ → แสดง Odd
- */
-
 import java.util.Scanner;
 
 public class lab13 {
-    public static void main(String[] args) {
+    public static void main(String[] agrs) {
         Scanner sc = new Scanner(System.in);
 
-        // ===== Input Section =====
-        System.out.print("Enter an integer: ");  
-        int num = sc.nextInt();
+    // ==ส่วนการป้อนข้อมูล==
+    System.out.print("Enter numbers:");
+    int sum = sc.nextInt();
 
-        // ===== Process & Output Section =====
-        if (num % 2 == 0) {
-            System.out.println("Even");   // เลขคู่
+    // ==ส่วนการประมวลผลและการแสดงผล==
+    if (sum % 2 == 0) {
+        System.out.println("Even numbers");
         } else {
-            System.out.println("Odd");    // เลขคี่
+            System.out.println("Odd numbers");
         }
-
         sc.close();
     }
+    
 }
