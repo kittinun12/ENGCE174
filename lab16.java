02db2323344d953ef3a3cdc6d753ec0e02186e0c
kittinun12 <hougohum33@gmail.com>
2025-11-22 16:19:41 +0700

    update2

--- lab16.java
+++ lab16.java
@@ -1,13 +1,26 @@
+/*
+ * โปรแกรมแสดงแม่สูตรคูณของตัวเลขที่ผู้ใช้ป้อน
+ * ผู้ใช้ป้อนค่า n (จำนวนเต็ม)
+ * โปรแกรมจะแสดง n x 1 ถึง n x 12
+ */
+
 import java.util.Scanner;
 
 public class lab16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();   // รับค่าแม่สูตรคูณ
+        // ===== Input Section =====
+        System.out.print("Enter number for multiplication table: ");
+        int n = sc.nextInt();
+
+        // ===== Process & Output Section =====
+        System.out.println("Multiplication Table of " + n);
 
         for (int i = 1; i <= 12; i++) {
             System.out.println(n + " x " + i + " = " + (n * i));
         }
+
+        sc.close();
     }
 }
