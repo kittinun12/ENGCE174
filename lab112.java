4591a8615736f73db847fe10b36821e009b3621d
kittinun12 <hougohum33@gmail.com>
2025-11-23 23:39:54 +0700

    updatehello

--- lab112.java
+++ lab112.java
@@ -1,31 +1,75 @@
 import java.util.Scanner;
 
 public class lab112 {
-
     public static void main(String[] args) {
-        Scanner readValue = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("Please enter the number of product types (N):");
+        
+        if (!scanner.hasNextInt()) {
+            System.out.println("Error: N must be an integer.");
+            scanner.close();
+            return;
+        }
+        int N = scanner.nextInt(); 
 
-        int N = readValue.nextInt();
-        int[] productID = new int[N];
-        int[] stockQty = new int[N];
+        
+        int[] productIDs = new int[N];
+        int[] stockQuantities = new int[N];
+
+        System.out.println("Please enter data for " + N + " products (ID and Quantity):");
+        
+        
         for (int i = 0; i < N; i++) {
-            productID[i] = readValue.nextInt();   
-            stockQty[i] = readValue.nextInt();    
+            
+            if (!scanner.hasNextInt()) {
+                System.out.println("Error: Invalid Product ID data at item " + (i + 1));
+                scanner.close();
+                return;
+        }
+            productIDs[i] = scanner.nextInt();
+            
+            
+            if (!scanner.hasNextInt()) {
+                System.out.println("Error: Invalid Quantity data at item " + (i + 1));
+                scanner.close();
+                return;
+        }
+            stockQuantities[i] = scanner.nextInt();
         }
 
-        int searchID = readValue.nextInt();
-        int foundIndex = -1;
-        for (int i = 0; i < N; i++) {
-            if (productID[i] == searchID) {
-                foundIndex = i;
-                break;
-            }
+        
+        System.out.println("Please enter the Product ID (Search ID) to look up:");
+        
+        if (!scanner.hasNextInt()) {
+            System.out.println("Error: Search ID must be an integer.");
+            scanner.close();
+            return;
         }
-
-        if (foundIndex != -1) {
-            System.out.println(stockQty[foundIndex]);
-        } else {
-            System.out.println("Product not found");
+        int searchID = scanner.nextInt();
+        
+        
+        int foundQuantity = -1; 
+        
+        for (int i = 0; i < N; i++) {
+            if (productIDs[i] == searchID) {
+                
+                foundQuantity = stockQuantities[i];
+                break; 
         }
     }
+
+        
+        System.out.print("\nOutput: ");
+        if (foundQuantity != -1) {
+            
+            System.out.println(foundQuantity);
+        } else {
+            
+            System.out.println("Product " + searchID + " not found");
+        }
+
+    scanner.close();
+}
 }
