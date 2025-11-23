1dcc5116ddb5a21cbc7646e98e79ccc1b301f8b6
kittinun12 <hougohum33@gmail.com>
2025-11-23 22:11:50 +0700

    updatebaba

--- lab17.java
+++ lab17.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class lab17 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();   // รับจำนวนตัวเลขที่จะรับต่อไป
-        int sum = 0;
-
-        for (int i = 0; i < N; i++) {
-            int value = sc.nextInt();   // รับค่าทีละตัว
-            sum += value;               // นำมาบวกสะสม
-        }
-
-        System.out.println(sum);         // แสดงผลรวม
+        Scanner reader = new Scanner(System.in);
+        int totalNumbers = reader.nextInt();
+        int result = 0;
+        int counter = 0;
+        do {
+            result += reader.nextInt();
+            counter++;
+        } while (counter < totalNumbers);
+        System.out.println(result);
     }
 }
