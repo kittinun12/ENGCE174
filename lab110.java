9cd0fc717115853b41cb7687dcd57c7ae61504ee
kittinun12 <hougohum33@gmail.com>
2025-11-23 22:30:23 +0700

    updatehaha

--- lab110.java
+++ lab110.java
@@ -2,24 +2,17 @@ import java.util.Scanner;
 
 public class lab110 {
 
-    // ฟังก์ชันคำนวณพื้นที่ (กว้าง x สูง)
     public static double calculateArea(double width, double height) {
         return width * height;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner getValue = new Scanner(System.in);
 
-        // รับค่าความกว้างและความสูง
-        double width = sc.nextDouble();
-        double height = sc.nextDouble();
-
-        // เรียกใช้ฟังก์ชัน calculateArea
+        double width = getValue.nextDouble();
+        double height = getValue.nextDouble();
         double area = calculateArea(width, height);
-
-        // แสดงผลลัพธ์
         System.out.println(area);
-
-        sc.close();
+        getValue.close();
     }
 }
