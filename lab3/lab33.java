fe1a2c851c8b69c8c09cfbeff6300a4a0f759ca1
kittinun12 <hougohum33@gmail.com>
2025-12-10 17:33:05 +0700

    update13

--- lab3/lab33.java
+++ lab3/lab33.java
@@ -1,28 +1,5 @@
 import java.util.Scanner;
 
-class User{
-    private String password;
-
-    public User(String p){
-        this.password = p;
-    }
-
-    public String getPassword() {return this.password;}
-
-    public void setPassword(String newPassword){
-        int len = newPassword.length(); //นับจำนวน newPassword เอาไปเก็บไว้ใน len
-
-        if( len >= 8 ) {
-            this.password = newPassword;
-
-            System.out.println("Password updated");
-        }
-        else{
-            System.out.println("Password is too short");
-        }
-    }
-}
-
 public class lab33{
     public static void main(String[] args) {
         Scanner getValue = new Scanner(System.in);
@@ -38,8 +15,27 @@ public class lab33{
         objectUser.setPassword(New);
 
         System.out.print(objectUser.getPassword());
-
-        //getValue.close();
-    }
+}
 }
 
+class User{
+    private String password;
+    public User(String p){
+        this.password = p;
+    }
+
+    public String getPassword() {return this.password;}
+
+    public void setPassword(String newPassword){
+        int len = newPassword.length();
+
+        if( len >= 8 ) {
+            this.password = newPassword;
+
+            System.out.println("Password updated");
+        }
+        else{
+            System.out.println("Password is too short");
+        }
+}
+}
