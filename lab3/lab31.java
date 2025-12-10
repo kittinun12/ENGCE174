399df24059d9c0954750ff0d4014684949a8e530
kittinun12 <hougohum33@gmail.com>
2025-12-10 15:17:47 +0700

    update12

---
+++ lab3/lab31.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class User {
+    private String username;
+
+    public User(String un) {
+        this.username = un;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+}
+
+public class lab31 {
+    public static void main(String[] args) {
+        Scanner getValue = new Scanner(System.in);
+
+        System.out.print("s: name:");
+        String n = getValue.nextLine();
+
+        User objectName = new User(n);
+
+        System.out.print(objectName.getUsername());
+    }
+}
